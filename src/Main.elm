e34b4aa9ad95e7a121c4b264ba2128440842f1bf
mikesaprykin <mikesaprykin@gmail.com>
2017-09-28 16:24:33 +0300

    feat(project): Migrated to create-elm-app

---
+++ src/Main.elm
@@ -0,0 +1,145 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
+
+---- MODEL ----
+
+
+type alias Model =
+    { sideBarOpen : Bool
+    , mainImage : String
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    (
+        { sideBarOpen = False
+        , mainImage = ""
+        }
+        , Cmd.none
+    )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = None
+      | ToggleSideBar
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        None ->
+            ( model, Cmd.none )
+
+        ToggleSideBar ->
+            ( { model | sideBarOpen = not model.sideBarOpen }, Cmd.none )
+
+
+---- VIEW ----
+
+conditionalClassToggle : Bool -> String -> String -> Attribute Msg
+conditionalClassToggle active activeClass notActiveClass =
+    class
+        (if active then
+            activeClass
+         else
+            notActiveClass
+        )
+
+
+overlayShowClass =
+    "overlay show-overlay"
+
+
+overlayHideClass =
+    "overlay hide-overlay"
+
+
+generateOverlayClass : Bool -> Attribute Msg
+generateOverlayClass overlayShow =
+    conditionalClassToggle overlayShow overlayShowClass overlayHideClass
+
+
+generateOverlay : Bool -> Html Msg
+generateOverlay show =
+    div [ generateOverlayClass show ]
+        []
+
+
+sideBarOpenClass =
+    "side-bar-open"
+
+
+sideBarClosedClass =
+    "side-bar-closed"
+
+
+generateSideBarClass : Bool -> Attribute Msg
+generateSideBarClass sideBarOpen =
+    conditionalClassToggle sideBarOpen sideBarOpenClass sideBarClosedClass
+
+
+generateSideBar : Bool -> List (Html Msg) -> Html Msg
+generateSideBar open elements =
+    div [ generateSideBarClass open ] elements
+
+
+emptySideBar : Model -> Html Msg
+emptySideBar model =
+    generateSideBar model.sideBarOpen []
+
+
+hamburgerOpen =
+    "hamburger active"
+
+
+hamburgerClosed =
+    "hamburger"
+
+hamburgerBar =
+    "hamburger__bar"
+
+hamburgerClassGenerator : Bool -> Attribute Msg
+hamburgerClassGenerator open =
+    conditionalClassToggle open hamburgerOpen hamburgerClosed
+
+
+generateHamburgerBars: List (Html Msg)
+generateHamburgerBars =
+    List.map (\n -> span [ class hamburgerBar ] []) (List.range 1 3)
+
+sideBarHamburger : Bool -> Html Msg
+sideBarHamburger sideBarOpen =
+    div [ onClick ToggleSideBar, hamburgerClassGenerator sideBarOpen ]
+        generateHamburgerBars
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "side-bar" ]
+        [ generateOverlay model.sideBarOpen
+        , emptySideBar model
+        , sideBarHamburger model.sideBarOpen
+        ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { view = view
+        , init = init
+        , update = update
+        , subscriptions = always Sub.none
+        }
